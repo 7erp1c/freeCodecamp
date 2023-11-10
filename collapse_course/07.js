e88000c9f147ea9fb030c0ab02579fe20f76ad91
7erp1c <ul_tray@bk.ru>
2023-11-11 00:30:17 +0300

    06+bracket_notation

---
+++ collapse_course/07.js
@@ -0,0 +1,10 @@
+//Word Blanks
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myAdverb + " " + myVerb + " " + myAdjective + " " + myNoun + "."; // Change this line
+// Only change code above this line
