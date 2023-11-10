2f59c9fa94f9728fe45c69eaf112df43ed48266b
7erp1c <ul_tray@bk.ru>
2023-11-10 23:47:03 +0300

    06+bracket_notation

--- collapse_course/06.js
+++ collapse_course/06.js
@@ -22,3 +22,10 @@ let firstLetterOfLastName = "";
 const lastName1 = "Lovelace";
 // Only change code below this line
 firstLetterOfLastName = lastName1[0]; // Change this line
+//Use Bracket Notation to Find the Nth Character in a String
+//Let's try to set thirdLetterOfLastName to equal the third letter
+// of the lastName variable using bracket notation.
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
