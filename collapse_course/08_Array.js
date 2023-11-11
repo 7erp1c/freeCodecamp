5ddaaccb54362ad0a7908d8a5a378954872b6ccb
7erp1c <ul_tray@bk.ru>
2023-11-11 21:04:06 +0300

    08_Array.js

--- collapse_course/08_Array.js
+++ collapse_course/08_Array.js
@@ -2,3 +2,6 @@
 // equal the first value of myArray using bracket notation.
 const myArray = [50, 60, 70];
 const myData = myArray[0];
+//Modify Array Data With Indexes
+const myArray1 = [18, 64, 99];
+myArray1[0] = 45;
