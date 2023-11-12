09e5926d5d753e742d295a4f82a2d509a7bb7d9e
7erp1c <ul_tray@bk.ru>
2023-11-12 22:09:33 +0300

    08_Array.js

--- collapse_course/08_Array.js
+++ collapse_course/08_Array.js
@@ -2,13 +2,20 @@
 // equal the first value of myArray using bracket notation.
 const myArray = [50, 60, 70];
 const myData = myArray[0];
-//Modify Array Data With Indexes
+//Modify Array Data With Indexes:
 const myArray1 = [18, 64, 99];
 myArray1[0] = 45;
-//Access Multi-Dimensional Arrays With Indexes
+//Access Multi-Dimensional Arrays With Indexes:
 const myArray2 = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
     [[10, 11, 12], 13, 14]];
 const myData = myArray2[2][1];
+//Manipulate Arrays With push Method:
+const myArray3 = [["John", 23], ["cat", 2]];
+myArray3.push(["dog", 3])
+//Manipulate Arrays With pop Method
+const myArray4 = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray4.pop();
+console.log(removedFromMyArray);
