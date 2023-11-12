a384feb58865b1db214a57532adc3b1bff369b76
7erp1c <ul_tray@bk.ru>
2023-11-12 22:26:32 +0300

    08_Array.js

--- collapse_course/08_Array.js
+++ collapse_course/08_Array.js
@@ -26,3 +26,15 @@ const removedFromMyArray5 = myArray5.shift();
 const myArray6 = [["John", 23], ["dog", 3]];
 myArray6.shift();
 myArray6.unshift(["Paul", 35]);
+//Create a shopping list in the variable myList.
+// The list should be a multi-dimensional array containing several sub-arrays.
+// The first element in each sub-array should contain a string with the name of the item.
+// The second element should be a number representing the quantity i.e.
+const myList1 =
+    [
+        ["Chocolate Bar", 15],
+        ["Coca colla", 14],
+        ["Lemonade", 13],
+        ["Chocolate Bar", 12],
+        ["Chocolate Bar", 11]
+    ];
