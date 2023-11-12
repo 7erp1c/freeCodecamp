b9416ffda40c49af961bc500bd6efa377d616a7e
7erp1c <ul_tray@bk.ru>
2023-11-13 02:35:33 +0300

    09_function.js.js

--- collapse_course/09_function.js
+++ collapse_course/09_function.js
@@ -3,3 +3,9 @@ function reusableFunction() {
     console.log("Hi World");
 }
 reusableFunction();
+
+//Passing Values to Functions with Arguments
+function functionWithArgs(param1, param2){
+    console.log (param1 + param2);
+}
+functionWithArgs(6,6);
