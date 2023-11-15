2c1beffa289008ee6775b7bbd450556a23244189
7erp1c <ul_tray@bk.ru>
2023-11-15 16:26:28 +0300

    ifElse

--- collapse_course/12 if_else_elseif_.js
+++ collapse_course/12 if_else_elseif_.js
@@ -8,8 +8,8 @@ function trueOrFalse(wasThatTrue) {
 }
 
 // Setup
-function testStrict(val) {
-    if (val === 7) { //оператор строгого равенства, а оператор == может сравнивать и число и строку
+function testStrict(val1) {
+    if (val1 === 7) { //оператор строгого равенства, а оператор == может сравнивать и число и строку
         // по значению 3 == "3" true; != неравен и преобразует типы данных, !== строго не равен
         return "Equal";
     }
@@ -17,9 +17,9 @@ function testStrict(val) {
 }
 testStrict(10);
 //Introducing Else Statements
-function testElse(val) {
+function testElse(val2) {
     let result = "";
-    if (val > 5 ) {
+    if (val2 > 5 ) {
         result = "Bigger than 5";
     }
     else
@@ -27,18 +27,30 @@ function testElse(val) {
     return result;
 }
 testElse(4);
+//Introducing Else If Statements
+function testElseIf(val3) {
+    if (val3 > 10) {
+        return "Greater than 10";
+    }
+    else if (val3 < 5) {
+        return "Smaller than 5";
+    }
+    else{
+        return "Between 5 and 10";
+    }}
+testElseIf(7);
 
 //Comparisons with the Logical And Operator
-function testLogicalAnd(val) {
-    if (val <=50 && val>=25) {
+function testLogicalAnd(val4) {
+    if (val4 <=50 && val4>=25) {
         return "Yes";
     }
     return "No";
 }
 testLogicalAnd(10);
 //Comparisons with the Logical Or Operator
-function testLogicalOr(val) {
-    if (val <10||val>20) {
+function testLogicalOr(val5) {
+    if (val5 <10||val5>20) {
         return "Outside";
     }
     return "Inside";
