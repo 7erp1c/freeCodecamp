c1264f2ebfea58844994f78ed1e7570c6464bd72
7erp1c <ul_tray@bk.ru>
2023-11-16 16:26:09 +0300

    _switch_.js.js

--- collapse_course/_switch_.js
+++ collapse_course/_switch_.js
@@ -1,4 +1,4 @@
-//Selecting from Many Options with Switch Statements
+//🥰🥰🥰Selecting from Many Options with Switch Statements
 function caseInSwitch(val) {
     let answer = "";
     // Only change code below this line
@@ -16,10 +16,31 @@ function caseInSwitch(val) {
             answer = "delta";
             break;
     }
-
-
     // Only change code above this line
     return answer;
 }
 
 caseInSwitch(1);
+//😀😀😀Adding a Default Option in Switch Statements
+function switchOfStuff(val1) {
+    let answer2 = "";
+    // Only change code below this line
+    switch(val1){
+        case "a":
+            answer2 = "apple";
+            break;
+        case "b":
+            answer2 = "bird";
+            break;
+        case "c":
+            answer2 = "cat";
+            break;
+        default:
+            answer2 = "stuff";
+            break;
+    }
+    // Only change code above this line
+    return answer2;
+}
+switchOfStuff(1);
+//😁😁😁
