8d01f61c3da8a27d6f52a7726eb500ea2f3157b2
7erp1c <ul_tray@bk.ru>
2023-11-16 22:59:00 +0300

    _switch_.js.js

--- collapse_course/_switch_.js
+++ collapse_course/_switch_.js
@@ -1,4 +1,4 @@
-//🥰🥰🥰Selecting from Many Options with Switch Statements
+//🥰🥰🥰 Selecting from Many Options with Switch Statements
 function caseInSwitch(val) {
     let answer = "";
     // Only change code below this line
@@ -21,11 +21,10 @@ function caseInSwitch(val) {
 }
 
 caseInSwitch(1);
-//😀😀😀Adding a Default Option in Switch Statements
-function switchOfStuff(val1) {
+//😀😀😀 Adding a Default Option in Switch Statements
+function switchOfStuff(val2) {
     let answer2 = "";
-    // Only change code below this line
-    switch(val1){
+    switch(val2){
         case "a":
             answer2 = "apple";
             break;
@@ -39,8 +38,52 @@ function switchOfStuff(val1) {
             answer2 = "stuff";
             break;
     }
-    // Only change code above this line
     return answer2;
 }
 switchOfStuff(1);
-//😁😁😁
+//😁😁😁 Multiple Identical Options in Switch Statements
+function sequentialSizes(val3) {
+    let answer3 = "";
+    switch(val3){
+        case 1:
+        case 2:
+        case 3:
+            answer3 = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer3 = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer3 = "High";
+            break;
+    }
+    return answer3;
+}
+sequentialSizes(1);
+//🤣🤣🤣Replacing If Else Chains with Switch
+function chainToSwitch(val4) {
+    let answer4 = "";
+    switch(val4){
+        case "bob":
+            answer4 = "Marley";
+            break;
+        case 42:
+            answer4 = "The Answer";
+            break;
+        case 1:
+            answer4 = "There is no #1";
+            break;
+        case 99:
+            answer4 = "Missed me by this much!";
+            break;
+        case 7:
+            answer4 = "Ate Nine";
+            break;
+    }
+    return answer4;
+}
+chainToSwitch(7);
