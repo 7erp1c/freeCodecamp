ebd58ce54f3caee88b5c3f67d7575b267c5da1f7
7erp1c <ul_tray@bk.ru>
2023-11-17 17:55:53 +0300

    13_objects_.js

---
+++ collapse_course/13_objects_.js
@@ -0,0 +1,10 @@
+//🧨🧨🧨 Build JavaScript Objects
+const myDog = {
+    // Only change code below this line
+    name: "Lucifer",
+    legs: 4,
+    tails: 1,
+    friends: ["Obi", "bark"]
+
+    // Only change code above this line
+};
