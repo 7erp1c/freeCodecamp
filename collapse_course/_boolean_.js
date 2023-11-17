06f38a730314137f1b681ff118e333c3f51b1b99
7erp1c <ul_tray@bk.ru>
2023-11-17 16:58:44 +0300

    _boolean_.js

--- collapse_course/_boolean_.js
+++ collapse_course/_boolean_.js
@@ -1,5 +1,14 @@
-//Returning Boolean Values from Functions
+//✨✨✨ Returning Boolean Values from Functions
 function isLess(a, b) {
     return a<b;
 }
 isLess(10, 15);
+
+//😀😀😀 Return Early Pattern for Functions
+function abTest(a1, b1) {
+    if (a1<0 || b1<0){
+        return undefined;
+    }
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+abTest(2,2);
