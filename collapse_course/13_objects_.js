790fd07f2080c66ee7eb6e11ef0c93b5ba72b121
7erp1c <ul_tray@bk.ru>
2023-11-18 15:33:53 +0300

    13_objects_.js

--- collapse_course/13_objects_.js
+++ collapse_course/13_objects_.js
@@ -18,13 +18,22 @@ const testObj = {
 // Only change code below this line
 const hatValue = testObj.hat;      // Change this line
 const shirtValue = testObj.shirt;    // Change this line
+
 //🎃🎃🎃 Получаем значение объекта с помощью квадратных скобок
-// Setup
 const testObj1 = {
     "an entree": "hamburger",
     "my side": "veggies",
     "the drink": "water"
 };
-// Only change code below this line
 const entreeValue = testObj1["an entree"];   // Change this line
 const drinkValue = testObj1["the drink"];    // Change this line
+
+//🎨🎨🎨 Присвоение значения объекта с помощью переменной
+// Setup
+const testObj2 = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+const playerNumber = 16;  // Change this line
+const player = testObj2[playerNumber];   // Change this line
