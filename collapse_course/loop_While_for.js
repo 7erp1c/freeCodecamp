ec24d253cc43ecf0978efc635401b51890e6cd83
7erp1c <ul_tray@bk.ru>
2023-11-21 17:27:28 +0300

    _While_.js

--- collapse_course/loop_While_for.js
+++ collapse_course/loop_While_for.js
@@ -27,3 +27,15 @@ let total = 0;
 for (let i = 0;i<myArr5.length; i++){
     total += myArr5[i];
 } console.log(total);
+// многомерные массивы:
+function multiplyAll(arr) {
+    let product = 1;
+    for(let i = 0;i < arr.length;i++){
+        for(let j = 0; j < arr[i].length;j++){
+            product *= arr[i][j];
+        }
+    }
+    return product;
+}
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
