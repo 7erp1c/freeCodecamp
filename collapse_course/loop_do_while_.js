fd320f9c2d832faa68460ead7be8f5dc33c6ca0d
7erp1c <ul_tray@bk.ru>
2023-11-22 15:42:58 +0300

    _loop_do_while

---
+++ collapse_course/loop_do_while_.js
@@ -0,0 +1,13 @@
+// СУть do...while: что каковы бы не были условия, цикл выполнит хотя бы одно дейстие в теле do{....}
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+
+do {
+    myArray.push(i);
+    i++;
+}
+while (i < 5) {
+
+}
