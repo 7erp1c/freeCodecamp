b287586ca416a53140d8567eb8c57ba90c0a64f0
7erp1c <ul_tray@bk.ru>
2023-11-22 18:05:17 +0300

    _loop_do_while

---
+++ collapse_course/рекурсия.js
@@ -0,0 +1,25 @@
+/* function multiply(arr, n) {
+    let product = 1;
+    for (let i = 0; i < n; i++) {
+      product *= arr[i];
+    }
+    return product;
+  }
+
+    function multiply(arr, n) {
+    if (n <= 0) {
+      return 1;
+    } else {
+      return multiply(arr, n - 1) * arr[n - 1];
+    }
+  }*/
+
+function sum(arr, n) {
+    // Only change code below this line
+    if (n <= 0) {
+        return 0;
+    } else {
+        return sum(arr, n - 1) + arr[n - 1];
+    }
+    // Only change code above this line
+}
