78a51881d5121e05fc99e33a8f36eb14065064b0
7erp1c <ul_tray@bk.ru>
2023-11-23 17:54:25 +0300

    Random.js

---
+++ collapse_course/Random.js
@@ -0,0 +1,7 @@
+//Функция которая возвращает рандомное число от 0 до (невключая)[1]
+
+function randomFraction() {
+    // Only change code below this line
+    return Math.random();
+    // Only change code above this line
+}
