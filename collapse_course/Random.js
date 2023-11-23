196c4e0f52884fde9f8a83379a9004fe1087b7ca
7erp1c <ul_tray@bk.ru>
2023-11-23 18:01:35 +0300

    Random.js

--- collapse_course/Random.js
+++ collapse_course/Random.js
@@ -1,7 +1,12 @@
-//Функция которая возвращает рандомное число от 0 до (невключая)[1]
+//🎈🎈🎈Функция которая возвращает рандомное число от 0 до (невключая)[1]
 
 function randomFraction() {
     // Only change code below this line
     return Math.random();
     // Only change code above this line
-}
+} console.log(randomFraction())
+
+// 🧱🧱🧱Рандомное число от 0 до 9 включистельно: Math.floor() округляет число, полученное с помощью Math.random.
+function randomWholeNum() {
+    return Math.floor(Math.random()*10);
+} console.log(randomWholeNum())
