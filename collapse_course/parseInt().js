5fc8d94c44b325570022c54acfb4cac575896653
7erp1c <ul_tray@bk.ru>
2023-11-24 15:11:05 +0300

    parseInt().js.js

---
+++ collapse_course/parseInt().js
@@ -0,0 +1,10 @@
+// 🎈🎈🎈Принимает число проверяет его на числа, возвращает числовое значение удаляя первые нули.
+function convertToInteger(str) {
+    return parseInt(str);
+}
+console.log(convertToInteger("56"));
+//🎈🎈🎈 Используйте parseInt() в функции, что бы она возвращала двоичное значение:
+function convertToInteger1(str,) {
+    return parseInt(str, 2);
+}
+console.log(convertToInteger1("10011"));
