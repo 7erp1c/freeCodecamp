e0e6c123e4587abd3ce3f4ec65ee81528e3b0631
7erp1c <ul_tray@bk.ru>
2023-11-24 15:22:14 +0300

    parseInt().js.js

--- collapse_course/Conditional_(Ternary)_Operator.js
+++ collapse_course/Conditional_(Ternary)_Operator.js
@@ -18,4 +18,4 @@ return a>b ? "a is greater" : "b is greater or equal";
 function checkEqual(a, b) {
     return a===b ? 'Equal' : 'Not Equal';
 }
-checkEqual(1, 2);
+console.log(checkEqual(10, 10));
