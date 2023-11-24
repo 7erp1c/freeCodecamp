fe19c3126c4f4d4119b70ce46cbd8009cc1079c0
7erp1c <ul_tray@bk.ru>
2023-11-24 15:21:11 +0300

    parseInt().js.js

---
+++ collapse_course/Conditional_(Ternary)_Operator.js
@@ -0,0 +1,21 @@
+// Условные операторы ( ?, : ), пример использования: (Условие) ? (Если True) : (Если False);
+/*
+Функция через if..else:
+function findGreater(a, b) {
+  if(a > b) {
+    return "a is greater";
+  }
+  else {
+    return "b is greater or equal";
+  }
+}
+Функция через условные операторы:
+function findGreater(a, b){
+return a>b ? "a is greater" : "b is greater or equal";
+}*/
+//Use the conditional operator in the checkEqual function to check if two numbers are equal or not.
+// The function should return either the string Equal or the string Not Equal.
+function checkEqual(a, b) {
+    return a===b ? 'Equal' : 'Not Equal';
+}
+checkEqual(1, 2);
