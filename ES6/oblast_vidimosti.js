020ea02f9ea3a4a69b45c8f24dc89fe57ffb4940
7erp1c <ul_tray@bk.ru>
2023-11-25 14:46:17 +0300

    oblast_vidimosti.js.js

--- ES6/oblast_vidimosti.js
+++ ES6/oblast_vidimosti.js
@@ -1,3 +1,4 @@
+// var объявляет переменные глобально, let  действует внутри поля, где обозначена.
 function checkScope() {
     let i = 'function scope';
     if (true) {
@@ -7,3 +8,13 @@ function checkScope() {
     console.log('Function scope i is: ', i);
     return i;
 } console.log(checkScope());
+// В переменной const нельзя изменить тип данных. Но можно поменять значение.
+const s = [5, 7, 2];
+function editInPlace() {
+    // Only change code below this line
+    s[0] = 2;
+    s[1] = 5; // Using s = [2, 5, 7] would be invalid
+    s[2] = 7;
+    // Only change code above this line
+}
+console.log(editInPlace());
