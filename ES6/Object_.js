82b24743f5d730618b4218a495fffe9c4266685e
7erp1c <ul_tray@bk.ru>
2023-11-26 16:27:53 +0300

    [деструктурирующее присваивание].js

--- ES6/Object_.js
+++ ES6/Object_.js
@@ -26,5 +26,22 @@ const high1Today = LOCAL_FORECAST.today.high;
 */
 const {today:{low:lowToday}} = LOCAL_FORECAST;
 const {today:{high:high1Today}} = LOCAL_FORECAST;
+//___________________________________Use Destructuring Assignment to Pass an Object as a Function's Parameters:
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+//const half = (stats) => (stats.max + stats.min) / 2.0;
+/*Используйте деструктурирующее присваивание в аргументе функции half,
+ чтобы отправлять только max и min внутри функции.*/
+const half = ({max, min}) => { //вместо использования точечной натации, делаем деструктурирующее присваивание в аргументе функции half
+    return (max + min)/ 2
+    //либо //const half = ({max, min}) =>  (max + min)/ 2
+};
+
 
 
