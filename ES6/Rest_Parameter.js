327bd2b2bd2addb5cb8ce6d2b396c6e523d1199b
7erp1c <ul_tray@bk.ru>
2023-11-26 16:28:21 +0300

    [,,...args].js

--- ES6/Rest_Parameter.js
+++ ES6/Rest_Parameter.js
@@ -21,3 +21,12 @@ const sum = (...args) => {
     }
     return total;
 }; console.log(sum(4,5,3))
+
+//используйте дестриктурирование с синтаксисом Rest, что бы получить новый массив,
+//за исключением первых двух элементов:
+function removeFirstTwo(list) {
+    const [,,...args] = list;
+    return args;
+}
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
