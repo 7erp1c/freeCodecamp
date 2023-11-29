edc8d883fe638f71fcf9726745460359e65e1aa4
7erp1c <ul_tray@bk.ru>
2023-11-29 20:47:52 +0300

    pole.js

--- collapse_course/ПОЛЕ.js
+++ collapse_course/ПОЛЕ.js
@@ -5,3 +5,15 @@ const reverseSeq = n => {
     }
     return arr;
 }; console.log(reverseSeq(3));
+
+let alphabet = "abcde";
+let len = alphabet.length;
+for (let i = 0; i <= len; i++) {// много
+    console.log(alphabet[i]);
+}
+for (let j = 1; j < len; j++) {//мало
+    console.log(alphabet[j]);
+}
+for (let k = 0; k < len; k++) {//good
+    console.log(alphabet[k]);
+}
