5bf6dc990ee550eeec34957f0361e05f0508a32e
7erp1c <ul_tray@bk.ru>
2023-11-29 22:26:04 +0300

    Array.js

--- Basic Data Structures/Array.js
+++ Basic Data Structures/Array.js
@@ -1,1 +1,7 @@
 let yourArray = [false,'some',2,undefined,66]; //1
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = 'r';//изменить второй элемент массива, любым другим кроме b;
+// Only change code above this line
+console.log(myArray);
