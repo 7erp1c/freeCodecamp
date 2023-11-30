790c91b6146270c2c00776b131416813cfa09237
7erp1c <ul_tray@bk.ru>
2023-11-30 20:25:54 +0300

    гтыршае(),push().js

---
+++ Basic Data Structures/splice().js
@@ -0,0 +1,9 @@
+//splice() - удаляет из любой точки массива (index - откуда начинаем, index2 - сколько удаляем), возвращает значения.
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];// получить 10
+// Only change code below this line
+let r1 = arr.shift();
+let r2 = arr.splice(3);
+
+
+// Only change code above this line
+console.log(arr);
