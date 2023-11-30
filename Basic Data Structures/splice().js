0f0717b09691b84cfe8f42c3816690d1441b4192
7erp1c <ul_tray@bk.ru>
2023-11-30 20:26:30 +0300

    splice().js

--- Basic Data Structures/splice().js
+++ Basic Data Structures/splice().js
@@ -3,7 +3,7 @@ const arr = [2, 4, 5, 1, 7, 5, 2, 1];// получить 10
 // Only change code below this line
 let r1 = arr.shift();
 let r2 = arr.splice(3);
-
+///
 
 // Only change code above this line
 console.log(arr);
