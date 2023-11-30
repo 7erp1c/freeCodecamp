37d0bb7ec7030ddc00d14256eeb974fda0dfba69
7erp1c <ul_tray@bk.ru>
2023-11-30 19:15:13 +0300

    гтыршае(),push().js

---
+++ Basic Data Structures/unshift();push().js
@@ -0,0 +1,10 @@
+//push() - добавляет элементы в конец
+//unshift() - добавляет элементы массива в начало
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift('I', 2, 'three');
+    arr.push(7, 'VIII', 9);
+    // Only change code above this line
+    return arr;
+}
+console.log(mixedNumbers(['IV', 5, 'six']));
