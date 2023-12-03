03cabb5899ec97ec0b92f5ff0af3b1ad9f285e4c
7erp1c <ul_tray@bk.ru>
2023-12-03 16:27:13 +0300

    slice().js

--- Basic Data Structures/slice().js
+++ Basic Data Structures/slice().js
@@ -1,8 +1,8 @@
 //Не изменяет массивб, а копирует его.
 //slice(index1,index2), где index1 - начало вырезаемого эл. index2 - конец, не включа.
 function forecast(arr) {
-
+//
     return arr.slice(2,4);
 }
-
+//
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
