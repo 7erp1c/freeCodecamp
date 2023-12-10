ca27031ebae26fff03500ee6e50d674489cc32bc
7erp1c <ul_tray@bk.ru>
2023-12-10 15:50:36 +0300

    split().toLoweCase().map=>();

--- 2 Basic Algorithm Scripting/Строка где все слова начинаются с верхнего регистра.js
+++ 2 Basic Algorithm Scripting/Строка где все слова начинаются с верхнего регистра.js
@@ -1,10 +1,11 @@
 
 function titleCase(str) {
-    let arr = str.split(" ");//Получаем из строки аррэйчик
-    return arr.map(a=>a[0].toUpperCase()+a.slice(1));//с помощью мапа, пробегаемся по всему масивчику,
+    let strLower = str.toLowerCase();
+    let arr = strLower.split(" ");
+    //Получаем из строки аррэйчик
+    return arr.map(a=>a[0].toUpperCase()+a.slice(1)).join(" ");//с помощью мапа, пробегаемся по всему масивчику,
     //даём каждому нулевому индексу Уппера, далее плюсуем слайсену которая отрезала первые буквы
     // и плюсуем Упперену и слайсену
-
 }
 
 console.log(titleCase("I'm a little tea pot"));
