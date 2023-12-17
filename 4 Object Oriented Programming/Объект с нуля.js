84996c94eae1c562e75da1d528ac8abc88f4ee09
7erp1c <ul_tray@bk.ru>
2023-12-17 17:30:54 +0300

    Object.js

---
+++ 4 Object Oriented Programming/Объект с нуля.js
@@ -0,0 +1,16 @@
+//Примеры:
+let dog = {
+    name: 'Alfa',
+    numLegs: 4
+};
+let duck = {
+    name: "Aflac",
+    numLegs: 2
+};
+//доступ к свойству объекта с помощью точечной нотации:
+let dog1 = {
+    name: "Spot",
+    numLegs: 4
+};
+console.log(dog1.numLegs);
+console.log(dog1.name);
