259348fff30940cf30bd9f6c5d9102a87db75752
7erp1c <ul_tray@bk.ru>
2023-12-18 19:04:38 +0300

    конструктор.prototype.key = свойство;

---
+++ 4 Object Oriented Programming/Свойства прототипа(для уменьшения дубликатов кода).js
@@ -0,0 +1,18 @@
+//Синтаксис добавление свойств через prototype конструктор.prototype.key = свойство;
+//Есть функция конструктор:
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+}
+let duck = new Bird("Donald");
+let canary = new Bird("Donald");
+//У всех копий будет одинаковый numLegs;
+//Bird.prototype.numLegs = 2;
+console.log(duck.numLegs);
+console.log(canary.numLegs);
+//создадим прототип для Dog:
+function Dog(name){
+    this.name = name;
+}
+Dog.prototype.numLegs =4;
+let terrier = new Dog('Grom');
